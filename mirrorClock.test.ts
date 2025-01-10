4cc24d4fd314906b2828fa79abf9399b4da10830
sabbond <sab@sabisme.uk>
2025-01-10 11:25:20 +0000

    Tweaks to initial implementation for readability extended tests to cover more cases and check 11 o'clock to 1 o'clock reflection Second version of reflect that works with total minutes instead of working with hours and minutes

--- mirrorClock.test.ts
+++ mirrorClock.test.ts
@@ -1,11 +1,40 @@
-import { mirrorTime } from "./mirrorClock";
+import { mirrorTime, mirrorTime2 } from "./mirrorClock";
+
+const testData = [
+    ['midday', '12:00', '12:00'],
+    ['1 minute past noon', '12:01', '11:59'],
+    ['half past twelve', '12:30', '11:30'],
+    ['quarter to one', '12:45', '11:15'],
+    ['a minute to one', '12:59', '11:01'],
+    ["one o'clock", '01:00', '11:00'],
+    ['quarter past three', '03:15', '08:45'],
+    ['twenty past five', '05:20', '06:40'],
+    ['four twenty nine', '04:29', '07:31'],
+    ['quarter to six', '05:45', '06:15'],
+    ['a minute to six', '05:59', '06:01'],
+    ["six o'clock", '06:00', '06:00'],
+    ["one minute past six", '06:01', '05:59'],
+    ['ten to eleven', '10:50', '01:10'],
+    ['ten to noon', '11:50', '12:10']
+];
 
 describe('mirrorTime', () => {
-    it.each([
-        ['midday', '12:00', '12:00'],
-        ['1 minute past noon', '12:01', '11:59'],
-        ['quarter past three', '03:15', '08:45']
-    ])('reflects time correctly when %s', (scenario: string, time: string, reflectedTime: string) => {
+    it.each(testData)('reflects time correctly when %s', (scenario: string, time: string, reflectedTime: string) => {
         expect(mirrorTime(time)).toEqual(reflectedTime);
+    })
+})
+
+describe('mirrorTime2', () => {
+    it.each(testData)('reflects time correctly when %s', (scenario: string, time: string, reflectedTime: string) => {
+        expect(mirrorTime2(time)).toEqual(reflectedTime);
+    })
+    it('returns the correct error message when no time supplied', () => {
+        expect(mirrorTime2('')).toEqual('invalid input');
+    })
+    it('returns the correct error message when bad length string supplied', () => {
+        expect(mirrorTime2('this is not a time string')).toEqual('invalid input');
+    })
+    it.each(['34:00', 'AB:CD', '13:00', '12:61', '12:-1', '01:99'])('returns the correct error message when time is %s', (input: string) => {
+        expect(mirrorTime2(input)).toEqual('invalid time');
     })
 })
--- mirrorClock.ts
+++ mirrorClock.ts
@@ -1,21 +1,57 @@
 const timeFormatRegex = /^(1[0-2]|0?[1-9]):([0-5]?[0-9])$/;
+const halfTotalMinutes = 60 * 6;
+// first cut of function
+// it works but it's funky and it isn't obvious how it works....
 export function mirrorTime(time: string): string {
-
-    if (!time || time.length != 5) return "invalid input";
-    if (!timeFormatRegex.test(time)) return "invalid time";
+    var invalid = invalidInput(time);
+    if (invalid) return invalid;
     // we know it's in the right format, get the hours and minutes
-    var hours = +time.split(':')[0];
-    var minutes = +time.split(':')[1];
+    var [hours, minutes] = extractTimeVariables(time);
     hours = reflectHours(hours, minutes);
     minutes = reflectMinutes(minutes);
     return `${('0' + hours).slice(-2)}:${('0' + minutes).slice(-2)}`
 }
 
+// calculate hours value, different when minutes == 0
+// if hours is 12 then reflected can be 0 or -1
+// so add to 12 when < 1 to get 12 or 11 in response
 function reflectHours(hours: number, minutes: number): number {
     var reflected = 6 + (6 - hours);
     if (minutes !== 0) { reflected-- };
-    return reflected < 1 ?  12 + reflected : reflected;
+    return reflected < 1 ? 12 + reflected : reflected;
 }
+// this is much simpler than the hours calculation as it's ok to return 0
 function reflectMinutes(minutes: number): number {
-    return 30 + (30 - minutes) === 60 ? 0 : 30 + (30 - minutes);
+    return (30 + (30 - minutes)) % 60;
+}
+
+// calculate based on total minutes since noon then format as a time
+export function mirrorTime2(time: string): string {
+    var invalid = invalidInput(time);
+    if (invalid) return invalid;
+    var [hours, minutes] = extractTimeVariables(time);
+    var minutesSinceNoon = ((hours % 12) * 60) + minutes;
+    // reflect time by calculating difference from 6 o'clock and inverting
+    var reflectedTotalMinutes = halfTotalMinutes + (halfTotalMinutes - minutesSinceNoon);
+    // now format total minutes back to a time
+    return timeStringFromTotalMinutes(reflectedTotalMinutes);
+}
+
+function invalidInput(time: string): string | undefined {
+    if (time.length != 5) return "invalid input";
+    if (!timeFormatRegex.test(time)) return "invalid time";
+}
+
+function extractTimeVariables(time: string): number[] {
+    var [hourString, minuteString] = time.split(':');
+    return [+hourString, +minuteString];
+}
+
+function timeStringFromTotalMinutes(totalMinutes: number): string {
+    var hours = Math.floor(totalMinutes / 60);
+    // we say 12 o'clock not 0 o'clock!
+    hours = hours ? hours : 12;
+    var minutes = totalMinutes % 60;
+    // simple format with leading 0 method
+    return `${('0' + hours).slice(-2)}:${('0' + (minutes)).slice(-2)}`
 }
